ba3a65fab2183d7ee8715f2dd192b7364d083fa3
shaziya01 <sk.shaziya9082@gmail.com>
2024-01-11 12:01:24 +0530

    I was just started angular so this is a simple page i made in angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+
+import { MovieComponent } from './movie/movie.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    MovieComponent
   ],
   imports: [
     BrowserModule,
